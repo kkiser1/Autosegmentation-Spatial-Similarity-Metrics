47e70dc6a2f410b976302c497279c029ad14aa69
kkiser1 <64553233+kkiser1@users.noreply.github.com>
2020-05-02 14:32:24 -0500

    Update segmentationMetrics_APIs.py

--- APIs/segmentationMetrics_APIs.py
+++ APIs/segmentationMetrics_APIs.py
@@ -29,19 +29,19 @@ def correctionsPathLength(auto, gt):
     
     Steps:
     1. Find pixels at the edge of the mask for both auto and gt
-    2. Count # pixels on the edge of gt that are not on the edge of auto
+    2. Count # pixels on the edge of gt that are not in the edge of auto
     '''
     
     # Check if auto and gt have same dimensions. If not, then raise a ValueError
     if auto.shape != gt.shape:
-        raise ValueError('Shape of auto and gt must be identical!!!')
+        raise ValueError('Shape of auto and gt must be identical!')
 
     # edge_auto has the pixels which are at the edge of the automated segmentation result
     edge_auto = util.getEdgeOfMask(auto)
     # edge_gt has the pixels which are at the edge of the ground truth segmentation
     edge_gt = util.getEdgeOfMask(gt)
     
-    # Count # pixels on the edge of gt that are on not on the edge of auto
+    # Count # pixels on the edge of gt that are on not in the edge of auto
     cpl = (edge_gt > edge_auto).astype(int).sum()
     
     return cpl 
@@ -53,12 +53,12 @@ def addedPathLength(auto, gt):
     
     Steps:
     1. Find pixels at the edge of the mask for gt
-    2. Count # pixels on the edge of gt that are not inside the mask in auto
+    2. Count # pixels on the edge of gt that are not in the auto mask volume
     '''
     
     # Check if auto and gt have same dimensions. If not, then raise a ValueError
     if auto.shape != gt.shape:
-        raise ValueError('Shape of auto and gt must be identical!!!')
+        raise ValueError('Shape of auto and gt must be identical!')
     
     # edge_gt has the pixels which are at the edge of the ground truth segmentation
     edge_gt = util.getEdgeOfMask(gt)
